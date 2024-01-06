66e2fa248861e176f28d554f1c057957d686ed82
ashoverflow <ashlyndennis29@gmail.com>
2024-01-06 15:51:46 -0700

    inputs v1

--- colour-palettes/src/components/Inputs.js
+++ colour-palettes/src/components/Inputs.js
@@ -11,6 +11,7 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { color } from '@mui/system';
 
 // TODO remove, this demo shouldn't need to reset the theme.
 
@@ -28,79 +29,104 @@ export default function Inputs() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Grid container component="main" sx={{ height: '100vh' }}>
+      <Grid container component="main" sx={{ height: '100vh' }} style={{backgroundColor: '#faf9f7'}}>
         <CssBaseline />
-        <Grid item xs={12} sm={8} md={7} component={Paper} elevation={6} square>
+        <Grid item xs={12} sm={8} md={6} component={Paper} square>
+          
           <Box
             sx={{
-              my: 8,
-              mx: 6,
+              my: 30,
+              mx: 2,
               display: 'flex',
               flexDirection: 'column',
               alignItems: 'center',
             }}
           >
-            <Typography component="h1" variant="h1" className=''>
-              Palette Pioneer
+            <Typography component="h4" variant="h4" style={{color: "#04baf7", fontFamily: 'sans-serif', fontWeight: 'bold'}}>
+                Manually Create a Palette
             </Typography>
-            {/* <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
               <TextField
+                sx={{color: "#04baf7"}}
                 margin="normal"
                 required
                 fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
+                id="phrase"
+                label="What Vibe are you looking for?"
+                name="phrase"
+                autoComplete="phrase"
                 autoFocus
+                color="secondary"
               />
+              <Typography component="h10" variant="h10" className='' sx={{display:"flex", flexDirection:"row", justifyContent:"center"}} style={{color: "#4f0128"}}>
+                  or
+              </Typography>
               <TextField
                 margin="normal"
                 required
                 fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
+                name="hexVal"
+                label="Enter a Hex Value"
+                type="hexVal"
+                id="hexVal"
+                color="secondary"
               />
               <Button
                 type="submit"
                 fullWidth
                 variant="contained"
                 sx={{ mt: 3, mb: 2 }}
+                style = {{backgroundColor: "#781e67"}}
               >
-                Sign In
+                GENERATE
               </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
-                </Grid>
-                <Grid item>
-                  <Link href="#" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
-              </Grid>
-            </Box> */}
+              
+            </Box>
           </Box>
+          
         </Grid>
+        
         <Grid
           item
-          xs={false}
-          sm={4}
-          md={5}
-          sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
-            backgroundRepeat: 'no-repeat',
-            backgroundColor: (t) =>
-              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-          }}
-        />
+          xs={12}
+          sm={8}
+          md={6}
+          component={Paper} 
+          square
+        >
+           <Box
+            sx={{
+              my: 30,
+              mx: 2,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Typography component="h4" variant="h4" style={{color: "#04baf7", fontFamily: 'sans-serif', fontWeight: 'bold'}}>
+                  ...or Generate a Random Palette
+              </Typography>
+            <Box
+                sx={{
+                  my: 14,
+                  mx: 2,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                }}
+              >
+
+              <Button
+                type="submit"
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                style = {{backgroundColor: "#781e67", padding:'20px'}}
+              >
+                GENERATE RANDOM PALETTE
+              </Button>
+            </Box>
+          </Box>
+        </Grid>
       </Grid>
     </ThemeProvider>
   );
